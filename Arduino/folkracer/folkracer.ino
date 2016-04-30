30a3f1063a33a26f8d53c32d4b4fb80ae706e2a2
Frode Lillerud <frode@lillerud.no>
2016-04-30 13:53:20 +0200

    Simple bluetooth works

--- Arduino/folkracer/folkracer.ino
+++ Arduino/folkracer/folkracer.ino
@@ -12,18 +12,46 @@
  * - EEPROM (on Teensy) for configuration
  */
 
+ //#include <SoftwareSerial.h> //For Bluetooth
+ //SoftwareSerial bluetooth(1,2);
+
+ const int LED_PIN = 13;
+
 void setup() {
   //USB Serial
   Serial.begin(115200);
 
+  //Onboard LED
+  pinMode(LED_PIN, OUTPUT);
+
+  //Bluetooth
+  Serial1.begin(9600);
+  Serial1.println("Bluetooth active via Serial1");
+  //bluetooth.begin(9600);
+  //bluetooth.println("Bluetooth active");
+
 }
 
-//int loopCount = 0;
+int loopCount = 0;
 void loop() {
-//  loopCount++;
+  loopCount++;
   
   Serial.print("In loop with 115200: ");
-//  Serial.println(loopCount);
+  Serial.println(loopCount);
 
-  delay(1000);
+  //digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+
+  //bluetooth.println("From Bluetooth");
+  //int bdata = bluetooth.read();
+  Serial1.println("From Bluetooth via Serial1");
+  int bdata = Serial1.read();
+  if (bdata == '1') {
+    digitalWrite(LED_PIN, LOW);
+  }
+  else
+  {
+    digitalWrite(LED_PIN, HIGH);
+  }
+
+  delay(100);
 }
